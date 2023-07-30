8648fdae0ee368af1437fe209e374b8c87c42e59
Rodgath <25994782+Rodgath@users.noreply.github.com>
2023-07-30 04:42:37 +0300

    Update gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const { src, dest, series, parallel, watch, task } = require('gulp');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const rename = require('gulp-rename');
+
+const compressJs = () => {
+  return src('src/*.js')
+          .pipe(dest('./demo/js')) // Send original script file to /demo
+          .pipe(dest('./dist')) // Send original script file to /dist
+          .pipe(babel({
+              presets: ['@babel/env']
+          }))
+          .pipe(uglify())
+          .pipe(rename( { suffix: '.min' } ))
+          .pipe(dest('./demo/js'))
+          .pipe(dest('./dist'))
+}
+
+const watchTask = () => {
+  watch(['./src/*.js', './demo/*.html'], { events: 'all' }, compressJs)
+}
+
+task('compress:js', compressJs);
+
+task('watch', watchTask);
+
+exports.default = compressJs;
