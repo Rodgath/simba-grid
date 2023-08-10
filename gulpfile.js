bd35b79e2eae239feb528cba4c6fdf9ca46f33af
Rodgath <25994782+Rodgath@users.noreply.github.com>
2023-08-10 18:13:21 +0300

    Add 'setBanner' task fucntion

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,36 @@ const { src, dest, series, parallel, watch, task } = require('gulp');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 const rename = require('gulp-rename');
+const header = require('gulp-header');
+const pkg = require('./package.json');
+
+/* Set banner for dist files */
+const setBanner = () => {
+	const banner = [
+    '/**',
+    ` * Simba Grid`,
+    ` * @name        ${pkg.name}`,
+    ` * @description ${pkg.description}`,
+    ` * @link        ${pkg.homepage}`,
+    ` * @author      ${pkg.author.name}, ${pkg.author.web}`,
+    ` * @version     v${pkg.version}`,
+    ` * @created     Jul 22, 2023`,
+    ` * @updated     Aug 04, ${new Date().getFullYear()}`,
+    ` * @copyright   Copyright (C) 2023-${new Date().getFullYear()}, ${pkg.author.name}`,
+    ` * @license     ${pkg.license}`,
+    ` * @licenseMIT  ${pkg.homepage}/blob/main/LICENSE`,
+    ` * @demoExample https://rodgath.github.io/simba-grid/demo/`,  
+    ' */',
+    ''
+  ].join('\n');
+  
+	return src('./dist/{css,js}/**/*.{js,css}')
+  .pipe(header(banner))
+  .pipe(dest('./dist'))
+}
+
+/* Set banner task */
+task('set:banner', () => setBanner())
 
 const compressJs = () => {
   return src('src/*.js')
@@ -16,12 +46,18 @@ const compressJs = () => {
         .pipe(dest('./dist/js'))
 }
 
+const buildTask = () => {
+  return series(compressJs, setBanner)
+}
+
 const watchTask = () => {
-  watch(['./src/*.js', './demo/*.html'], { events: 'all' }, compressJs)
+  watch(['./src/*.js', './demo/*.html'], { events: 'all' }, buildTask)
 }
 
 task('compress:js', compressJs);
 
 task('watch', watchTask);
 
-exports.default = compressJs;
+task('build', buildTask);
+
+exports.default = buildTask();
